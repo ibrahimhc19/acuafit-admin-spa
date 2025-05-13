b726ad3f28bb70865606585df9981b1bed0dc3e0
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-05-13 08:54:00 -0500

    Login terminado

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
-use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('auth/redirect/google', [GoogleController::class, 'redirect']);
-Route::get('auth/callback/google', [GoogleController::class, 'callback']);
+// Route::get('auth/redirect/google', [GoogleController::class, 'redirect']);
+// Route::get('auth/callback/google', [GoogleController::class, 'callback']);
