df609cc82fa58adf9915f898cb68ab6819bc0c46
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-05-20 16:50:11 -0500

    Estudiante modelo, controlador, IU formulario y de Success

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\EstudianteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/estudiantes', [EstudianteController::class,'index'])->middleware('auth:sanctum');
+Route::get('/estudiante/{id}', [EstudianteController::class,'show'])->middleware('auth:sanctum');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\GoogleController;
+use App\Models\Estudiante;
+use App\Http\Controllers\EstudianteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/exito', function () {
+//     return view('success');
+// });
 
 // Route::get('auth/redirect/google', [GoogleController::class, 'redirect']);
 // Route::get('auth/callback/google', [GoogleController::class, 'callback']);
+
+Route::get('/inscripciones/crear', [EstudianteController::class, 'create'])->name('inscripciones.create');
