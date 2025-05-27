fc2fd8a999d9b243433a18d02c5ff293ca37bd13
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-05-27 09:22:26 -0500

    Limpieza de dependencias 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { Provider } from 'react-redux';
-import { store } from './store.ts'
 
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render( <App /> )
--- frontend/src/pages/Students.tsx
+++ frontend/src/pages/Students.tsx
@@ -1,5 +1,3 @@
-import DataTable from "datatables.net-react";
-import DT from "datatables.net-dt";
 import type { Estudiante } from "../types";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -7,10 +5,9 @@ axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 // import { Trash2 } from 'lucide-react';
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-DataTable.use(DT);
 
 export default function StudentsPage() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [tableData, setTableData] = useState<Estudiante[][]>([]);
   const apiUrl = import.meta.env.VITE_APP_API_URL;
   const getData = async () => {
@@ -62,45 +59,7 @@ export default function StudentsPage() {
       </h1>
 
       <div className="overflow-x-auto bg-white shadow-xl rounded-lg p-2">
-        <DataTable
-          data={tableData}
-          className="min-w-full w-full display compact nowrap order-column"
-          options={{
-            columnDefs: [
-              {
-                targets: "_all",
-                className: "dt-left",
-              },
-            ],
-            language: {
-              search: "Buscar:",
-              lengthMenu: "Mostrar _MENU_ registros por página",
-              zeroRecords: "No se encontraron estudiantes que coincidan",
-              info: "Mostrando página _PAGE_ de _PAGES_ (de _TOTAL_ registros)",
-              infoEmpty: "No hay registros disponibles",
-              infoFiltered: "(filtrado de _MAX_ registros totales)",
-              paginate: {
-                first: "Primero",
-                last: "Último",
-                next: "Siguiente",
-                previous: "Anterior",
-              },
-            },
-            lengthMenu: [
-              [10, 15, 25, 50, 100, -1],
-
-              ["10", "15", "25", "50", "100", "Todos"],
-            ],
-            // Podrías querer activar la paginación o cambiar el número de filas por defecto
-            pageLength: 15,
-            // paging: true, // Activa la paginación (generalmente activada por defecto)
-            // searching: true, // Activa la búsqueda (generalmente activada por defecto)
-
-            // Para un diseño responsive más avanzado, necesitarías la extensión DataTables Responsive:
-            // responsive: true,
-            // (Esto requiere importar y registrar la extensión 'datatables.net-responsive-dt')
-          }}
-        >
+        <table>
           <thead className="bg-[#352c6f] text-white">
             <tr>
               <th className="px-5 py-3 text-sm font-semibold">#</th>
@@ -114,7 +73,7 @@ export default function StudentsPage() {
               <th className="px-5 py-3 text-sm font-semibold">Horario</th>
             </tr>
           </thead>
-        </DataTable>
+        </table>
       </div>
     </div>
   );
