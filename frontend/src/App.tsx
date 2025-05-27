d6aacee55fc7f07ac8fc352fb78fa61c8c66b1a2
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-05-27 14:26:30 -0500

    Aplicado el login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Students from "./pages/Students.tsx";
-import Login from "./pages/Login.tsx";
+// import Login from "./pages/Login.tsx";
 import Locations from "./pages/Locations.tsx";
 import Accounting from "./pages/Accounting.tsx";
 import Schedules from "./pages/Schedules.tsx";
@@ -9,6 +9,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home.tsx";
 import Parents from "./pages/Parents.tsx";
 import Payments from "./pages/Payments.tsx";
+import { LoginForm } from "./components/login-form.tsx";
 
 export default function App() {
 
@@ -27,7 +28,8 @@ export default function App() {
             <Route path="/dashboard" element={<Home />} />
             </Route>
             <Route path="*" element={<NotFoundPage />} />
-            <Route path="login" element={<Login />} />
+            {/* <Route path="login" element={<Login />} /> */}
+            <Route path="login" element={<LoginForm />} />
           </Routes>
       </Router>
     </div>
